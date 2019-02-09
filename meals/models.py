730a158fbc5200333b869755bb924893199dda60
jso4967 <jso4967@gmail.com>
2019-02-09 21:33:29 +0900

    Update models.py
    Add default args at models.CharField() & __str__()

--- meals/models.py
+++ meals/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Meal(models.Model):
-    meal_name = models.CharField(max_length=200)
-    restaurant_name = models.CharField(max_length=200)
+    meal_name = models.CharField(max_length=200, default='default')
+    restaurant_name = models.CharField(max_length=200, default='default')
     # price_range =
     # satisfaction =
     # visit_frequency =
     # location =
 
     def __str__(self):
-        pass
+        return self.meal_name
