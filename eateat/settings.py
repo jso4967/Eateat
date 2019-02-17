fb5f178bf50e9960ef12ed7a14fdaf146d26d2c8
jso4967 <33389418+jso4967@users.noreply.github.com>
2019-02-17 20:27:19 +0900

    Update settings.py
    For css

--- eateat/settings.py
+++ eateat/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFIELS_DIR = [os.path.join(BASE_DIR, 'static')]
+# STATICFIELS_DIR = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
