13a4b94b2982163b24ea9e3921a09e5fb78ef16e
GoBeromsu <37897508+GoBeromsu@users.noreply.github.com>
2019-02-18 17:41:05 +0900

    form 수정

--- meals/forms.py
+++ meals/forms.py
@@ -6,11 +6,11 @@ class MealForm(forms.ModelForm):
     class Meta:
         model = models.Meal
         fields = ('meal_name','meal_photo','restaurant_name','price_range','satisfaction',)
-
+        price_range = forms.ChoiceField(choices=[('높음', '높음'), ('중간', '중간'), ('낮음', '낮음')]),
+        satisfaction = forms.ChoiceField(choices=[('높음', '높음'), ('중간', '중간'), ('낮음', '낮음')]),
         widgets = {
+
             'meal_name' : forms.TextInput(attrs={'class':'form-control'}),
             'restaurant_name' : forms.TextInput(attrs={'class':'form-control'}),
-            'price_range' : forms.TextInput(attrs={'class':'form-control'}),
-            'satisfaction' : forms.TextInput(attrs={'class':'form-control'}),
 
         }
