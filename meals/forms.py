9a6ccd237d7a040382b874660f250fe36f21ed43
ajang369 <37576725+ajang369@users.noreply.github.com>
2019-02-18 20:48:01 +0900

    commit

--- meals/forms.py
+++ meals/forms.py
@@ -1,13 +1,16 @@
-# from django import forms
-# from .models import models.Meal
-#
-# class MealForm(forms.ModelForm):
-#
-#     class Meta:
-#         model = models.meal
-#         fields = ('meal_name','meal_photo','restaurant_name','price_range','satisfaction',)
-#
-#         widgets = {
-#             'title' : forms.
-#         }
-#
+from django import forms
+from . import models
+
+class MealForm(forms.ModelForm):
+
+    class Meta:
+        model = models.Meal
+        fields = ('meal_name','meal_photo','restaurant_name','price_range','satisfaction',)
+        price_range = forms.ChoiceField(choices=[('높음', '높음'), ('중간', '중간'), ('낮음', '낮음')]),
+        satisfaction = forms.ChoiceField(choices=[('높음', '높음'), ('중간', '중간'), ('낮음', '낮음')]),
+        widgets = {
+
+            'meal_name' : forms.TextInput(attrs={'class':'form-control'}),
+            'restaurant_name' : forms.TextInput(attrs={'class':'form-control'}),
+
+        }
