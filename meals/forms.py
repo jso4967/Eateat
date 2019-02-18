f9b6c7183aa602f21ed8760099fe0fab6c70bdaf
jso4967 <33389418+jso4967@users.noreply.github.com>
2019-02-18 20:02:28 +0900

    Revert "Merge branch 'Jeon-Se-Ok2'"
    This reverts commit c31137a5aa522563f7c0e9c3ddb9ce568961b82c, reversing
    changes made to 933fbca06dcc528cb5a22c913e18629a98a54381.

--- meals/forms.py
+++ meals/forms.py
@@ -1,16 +1,13 @@
-from django import forms
-from . import models
-
-class MealForm(forms.ModelForm):
-
-    class Meta:
-        model = models.Meal
-        fields = ('meal_name','meal_photo','restaurant_name','price_range','satisfaction',)
-
-        widgets = {
-            'meal_name' : forms.TextInput(attrs={'class':'form-control'}),
-            'restaurant_name' : forms.TextInput(attrs={'class':'form-control'}),
-            'price_range' : forms.TextInput(attrs={'class':'form-control'}),
-            'satisfaction' : forms.TextInput(attrs={'class':'form-control'}),
-
-        }
+# from django import forms
+# from .models import models.Meal
+#
+# class MealForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = models.meal
+#         fields = ('meal_name','meal_photo','restaurant_name','price_range','satisfaction',)
+#
+#         widgets = {
+#             'title' : forms.
+#         }
+#
