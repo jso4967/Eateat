77adec3739a46e1ccf4e49d39ffe8a88d74f6db7
GoBeromsu <37897508+GoBeromsu@users.noreply.github.com>
2019-02-18 16:25:39 +0900

    20190218

--- meals/views.py
+++ meals/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import (CreateView, ListView, DetailView, DeleteView, TemplateView,UpdateView,)
-from . import models
 from django.urls import (reverse, reverse_lazy)
+from . import models
+from . import forms
 # Create your views here.
 
 
@@ -16,8 +17,13 @@ class IndexView(TemplateView):
         return context
 
 class MealCreateView(CreateView):
-    fields = ("meal_photo", "meal_name", "restaurant_name", "price_range", "satisfaction")
+
     model = models.Meal
+    form_class = forms.MealForm
+
+    def form_valid(self,form):
+        form.save()
+        return super(MealCreateView,self).form_valid(form)
 
     def get_absolute_url(self):
         return reverse('index')
@@ -53,5 +59,3 @@ class MealDelete(DeleteView):
 class MealUpdateView(UpdateView):
     fields = ("meal_photo", "meal_name", "restaurant_name", "price_range", "satisfaction")
     model = models.Meal
-
-
